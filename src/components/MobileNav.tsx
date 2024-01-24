35e44d71327254cfc8937cd6c64ae98f1f17bc8a
KudoExtase810 <alaeddino4892@gmail.com>
2024-01-24 21:03:13 +0100

    added mobile version of the navbar

---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import clsx from "clsx";
+import React from "react";
+import { Link, usePathname } from "@/lib/navigation";
+
+interface MobileNavProps {
+    links: {
+        label: string;
+        href: string;
+    }[];
+    showSidebar: boolean;
+    setShowSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const MobileNav = ({ links, showSidebar, setShowSidebar }: MobileNavProps) => {
+    const pathname = usePathname();
+
+    const handleClose = () => setShowSidebar(false);
+    return (
+        <>
+            {/* Overlay */}
+            {showSidebar && (
+                <div
+                    className="fixed inset-0 bg-black opacity-50 z-10 md:hidden"
+                    onClick={handleClose}
+                ></div>
+            )}
+            {/* Sidebar */}
+            <div
+                className={clsx(
+                    "fixed h-screen z-20 right-0 top-0 bg-base-200 transition-transform duration-500",
+                    !showSidebar && "translate-x-full"
+                )}
+            >
+                <ul className="flex flex-col gap-2 text-center py-8 px-16">
+                    {links.map((link) => (
+                        <li key={link.label}>
+                            <Link
+                                className={clsx(
+                                    pathname !== link.href && "text-primary"
+                                )}
+                                href={link.href}
+                            >
+                                {link.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
+};
+
+export default MobileNav;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,52 +5,75 @@ import { Link, usePathname, useRouter } from "@/lib/navigation";
 import clsx from "clsx";
 import { useLocale, useTranslations } from "next-intl";
 import Image from "next/image";
+import { useState } from "react";
+import { RiMenu3Fill } from "react-icons/ri";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
     const t = useTranslations("Navbar");
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    const pathname = usePathname();
+    const isHome = pathname === "/";
 
     const links = [
         { label: t("home"), href: "/" },
         { label: t("about-us"), href: "/about" },
+        { label: t("contact-us"), href: "https://develop.sspacess.com/form" },
+        { label: t("login"), href: "https://develop.sspacess.com" },
     ];
-    const pathname = usePathname();
-    const isHome = pathname === "/";
 
+    const openSidebar = () => setShowSidebar(true);
     return (
-        <header
-            className={clsx(
-                "navbar bg-base-100 px-4 md:px-8",
-                isHome && "absolute top-0 bg-opacity-50"
-            )}
-        >
-            <div className="flex-1">
-                <Link href="/">
-                    <Image
-                        width={175}
-                        height={30}
-                        src="https://img.logoipsum.com/255.svg"
-                        alt="logo"
-                    />
-                </Link>
-            </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1 font-transcend">
-                    {links.map((link) => (
-                        <li key={link.label}>
-                            <Link
-                                className={clsx(
-                                    pathname !== link.href && "text-primary"
-                                )}
-                                href={link.href}
-                            >
-                                {link.label}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-                <LocaleSwitcher />
-            </div>
-        </header>
+        <>
+            <header
+                className={clsx(
+                    "navbar bg-base-100 px-4 md:px-8",
+                    isHome && "absolute top-0 bg-opacity-50"
+                )}
+            >
+                <div className="flex-1">
+                    <Link href="/">
+                        <Image
+                            width={175}
+                            height={30}
+                            src="https://img.logoipsum.com/255.svg"
+                            alt="logo"
+                        />
+                    </Link>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1 font-transcend max-md:hidden">
+                        {links.map((link) => (
+                            <li key={link.label}>
+                                <Link
+                                    className={clsx(
+                                        pathname !== link.href && "text-primary"
+                                    )}
+                                    href={link.href}
+                                >
+                                    {link.label}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+
+                    <LocaleSwitcher />
+                    <button
+                        onClick={openSidebar}
+                        className="md:hidden ml-2 text-primary"
+                    >
+                        <RiMenu3Fill size={28} />
+                    </button>
+                </div>
+            </header>
+            {/* Mobile nav sidebar */}
+            <MobileNav
+                links={links}
+                showSidebar={showSidebar}
+                setShowSidebar={setShowSidebar}
+            />
+        </>
     );
 };
 
