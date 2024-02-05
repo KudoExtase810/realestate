2b222b5965c7d2c8491cb4a3a1c094396b38ad27
KudoExtase810 <alaeddino4892@gmail.com>
2024-02-05 09:52:52 +0100

    fixed hero btn issue and slowed video pb

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,9 +1,20 @@
+"use client";
+
 import { Link } from "@/lib/navigation";
 import { useTranslations } from "next-intl";
+import { useEffect, useRef } from "react";
 import { FaChevronRight } from "react-icons/fa";
 
 const Hero = () => {
     const t = useTranslations("Home");
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.playbackRate = 0.75;
+        }
+    }, []);
+
     return (
         <section className="hero min-h-screen relative">
             <div className="hero-overlay bg-black bg-opacity-60 z-[2]"></div>
@@ -15,13 +26,17 @@ const Hero = () => {
                     <p className="mb-5 sm:text-lg mx-auto lg:max-w-lg text-secondary-content">
                         {t("description")}
                     </p>
-                    <Link href="/about" className="group gold-effect mx-auto">
+                    <Link
+                        href="https://develop.sspacess.com/form"
+                        className="group gap-1.5 rtl:flex-row-reverse gold-effect mx-auto max-w-96 max-sm:w-[90vw] max-sm:h-16"
+                    >
                         <span className="z-10">{t("button")}</span>
-                        <FaChevronRight className="z-10 transition-transform duration-300 group-hover:translate-x-3" />
+                        <FaChevronRight className="z-10 transition-transform duration-300 group-hover:translate-x-3 " />
                     </Link>
                 </div>
             </div>
             <video
+                ref={videoRef}
                 src="/videos/hero.mp4"
                 autoPlay
                 muted
